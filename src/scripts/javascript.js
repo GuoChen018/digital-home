53f90a2c0de92b77d6ac42f3b5be69cef4f7b47b
Guo Chen <guoc0818@gmail.com>
2024-04-08 10:42:33 -0400

    Fix responsiveness

--- src/scripts/javascript.js
+++ src/scripts/javascript.js
@@ -29,19 +29,3 @@ svgItems.forEach(item => {
 
   });
 });
-
-// Enable hidden nav bar
-{
-  const nav = document.querySelector("nav");
-  let lastScrollY = window.scrollY;
-
-  window.addEventListener("scroll", () => {
-    if (lastScrollY < window.scrollY) {
-      nav.classList.add("nav--hidden");
-    } else {
-      nav.classList.remove("nav--hidden");
-    }
-
-    lastScrollY = window.scrollY;
-  });
-}
