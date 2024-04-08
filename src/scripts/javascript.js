fe2e21b5489c00068d0e4ca4aa8924fd1c223c7e
Guo Chen <guoc0818@gmail.com>
2024-04-08 00:03:13 -0400

    Design responsiveness

--- src/scripts/javascript.js
+++ src/scripts/javascript.js
@@ -29,3 +29,19 @@ svgItems.forEach(item => {
 
   });
 });
+
+// Enable hidden nav bar
+{
+  const nav = document.querySelector("nav");
+  let lastScrollY = window.scrollY;
+
+  window.addEventListener("scroll", () => {
+    if (lastScrollY < window.scrollY) {
+      nav.classList.add("nav--hidden");
+    } else {
+      nav.classList.remove("nav--hidden");
+    }
+
+    lastScrollY = window.scrollY;
+  });
+}
